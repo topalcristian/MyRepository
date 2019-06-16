44aae187944e17548029b057c4d910677ba3fe78
LaurenzOM <45855127+LaurenzOM@users.noreply.github.com>
2019-06-16 19:27:05 +0200

    Add files via upload

---
+++ Computer Science /BankAccount/modifyBA.java
@@ -0,0 +1,56 @@
+import java.lang.IllegalArgumentException;
+
+		public class modifyBA{
+			
+			private double balance;
+
+			public modifyBA(double amount) throws IllegalArgumentException{
+				balance = amount;
+					// if balance is negative -> exception 
+					if(balance<0){
+						throw new IllegalArgumentException("Your balance is negative!");
+					}
+			}
+
+			public void deposit(double amount) throws IllegalArgumentException{
+				balance += amount;
+				// if deposit is negative -> exception
+				if(amount<0){
+					throw new IllegalArgumentException("You cannot deposit a negative amount!");
+				}
+			}
+
+			public void withdraw(double amount) throws IllegalArgumentException{
+				// if you withdraw a greater amount of money than you have on you bankaccount -> exception
+				if(amount>balance){
+					throw new IllegalArgumentException("You withdraw more money than you have on your bankaccount!");
+				}
+				 balance = balance - amount;
+			}
+			
+			public static void main(String[] args){
+
+				try{
+					// balance exception
+					modifyBA account1 = new modifyBA(-200);
+					account1.deposit(20);
+					account1.withdraw(20);
+				}catch(IllegalArgumentException h){
+						System.out.println(h);
+				}try{
+					// deposit exception
+					modifyBA account1 = new modifyBA(200);
+					account1.deposit(-20);
+					account1.withdraw(20);
+				}catch(IllegalArgumentException h){
+						System.out.println(h);
+				}try{
+					// withdraw exception
+					modifyBA account1 = new modifyBA(200);
+					account1.deposit(20);
+					account1.withdraw(2000);
+				}catch(IllegalArgumentException h){
+						System.out.println(h);
+				}
+			}			
+		}
